b16deac310e80ce49a2600d9727edb1a6648c547
Syed Ahzam Hussain <ahzamsyed6@gmail.com>
2025-07-11 01:23:07 +0500

    admindashboardcomplete

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -11,6 +11,54 @@ const TaskList = () => {
         <h2 className='mt-5 text-2xl font-semibold'>Make A Project</h2>
         <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quasi minus voluptatem exercitationem nulla amet.</p>
       </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-green-400 rounded-xl p-5'>
+        <div className='flex justify-between items-center'>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h3 className='text-sm'>7-July-2025</h3>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make A Project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quasi minus voluptatem exercitationem nulla amet.</p>
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-blue-400 rounded-xl p-5'>
+        <div className='flex justify-between items-center'>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h3 className='text-sm'>7-July-2025</h3>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make A Project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quasi minus voluptatem exercitationem nulla amet.</p>
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl p-5'>
+        <div className='flex justify-between items-center'>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h3 className='text-sm'>7-July-2025</h3>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make A Project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quasi minus voluptatem exercitationem nulla amet.</p>
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-green-400 rounded-xl p-5'>
+        <div className='flex justify-between items-center'>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h3 className='text-sm'>7-July-2025</h3>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make A Project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quasi minus voluptatem exercitationem nulla amet.</p>
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl p-5'>
+        <div className='flex justify-between items-center'>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h3 className='text-sm'>7-July-2025</h3>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make A Project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quasi minus voluptatem exercitationem nulla amet.</p>
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-green-400 rounded-xl p-5'>
+        <div className='flex justify-between items-center'>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h3 className='text-sm'>7-July-2025</h3>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make A Project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quasi minus voluptatem exercitationem nulla amet.</p>
+      </div>
     </div>
   )
 }
---
+++ src/components/other/AllTask.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-40 overflow-auto'>
+      <div className='bg-red-400 mb-2 py-2 px-4 justify-between flex rounded'>
+          <h2>Sarthak</h2>
+          <h3>Make A UI Design</h3>
+          <h5>Status</h5>
+      </div>
+            <div className='bg-blue-400 mb-2 py-2 px-4 justify-between flex rounded'>
+          <h2>Sarthak</h2>
+          <h3>Make A UI Design</h3>
+          <h5>Status</h5>
+      </div>
+            <div className='bg-green-400 mb-2 py-2 px-4 justify-between flex rounded'>
+          <h2>Sarthak</h2>
+          <h3>Make A UI Design</h3>
+          <h5>Status</h5>
+      </div>
+            <div className='bg-purple-400 mb-2 py-2 px-4 justify-between flex rounded'>
+          <h2>Sarthak</h2>
+          <h3>Make A UI Design</h3>
+          <h5>Status</h5>
+      </div>
+            <div className='bg-yellow-400 mb-2 py-2 px-4 justify-between flex rounded'>
+          <h2>Sarthak</h2>
+          <h3>Make A UI Design</h3>
+          <h5>Status</h5>
+      </div>
+
+    </div>
+  )
+}
+
+export default AllTask
